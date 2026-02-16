7765fe13301391c47e20de590a6fbbf53dff253c
Steven Meyer <stanmeye@iu.edu>
2026-02-16 18:30:26 -0500

    Clean up code and PEP-8

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,13 +1,24 @@
 import __main__
+
 from .data_engine import process_translation_rankings
 
+
 def list_authors(by_languages=False, alias=False):
-    # Access the dataframes loaded in your notebook
-    metadata = getattr(__main__, 'metadata', None)
-    authors = getattr(__main__, 'authors', None)
-    
-    if metadata is not None and authors is not None:
-        if by_languages and alias:
-            return process_translation_rankings(metadata, authors, alias_only=True)
-    
+    """
+    Return a list of authors or aliases ranked by number of languages,
+    using dataframes loaded in the active notebook.
+    """
+    metadata = getattr(__main__, "metadata", None)
+    authors = getattr(__main__, "authors", None)
+
+    if metadata is None or authors is None:
+        return []
+
+    if by_languages and alias:
+        return process_translation_rankings(
+            metadata,
+            authors,
+            alias_only=True,
+        )
+
     return []
--- tt_gutenberg/data_engine.py
+++ tt_gutenberg/data_engine.py
@@ -1,15 +1,29 @@
 import pandas as pd
 
-def process_translation_rankings(metadata_df, authors_df, alias_only=True):
-    # Merge and count unique languages
-    merged = pd.merge(metadata_df, authors_df, on='gutenberg_author_id')
-    
-    col_to_rank = 'alias' if alias_only else 'author'
-    
+
+def process_translation_rankings(
+    metadata_df,
+    authors_df,
+    alias_only=True,
+):
+    """
+    Merge metadata and author data, then rank authors or aliases
+    by the number of unique languages they have been translated into.
+    """
+    merged = pd.merge(
+        metadata_df,
+        authors_df,
+        on="gutenberg_author_id",
+    )
+
+    col_to_rank = "alias" if alias_only else "author"
+
     rankings = (
-        merged.dropna(subset=[col_to_rank])
-        .groupby(col_to_rank)['language']
+        merged
+        .dropna(subset=[col_to_rank])
+        .groupby(col_to_rank)["language"]
         .nunique()
         .sort_values(ascending=False)
     )
+
     return list(rankings.index)
